6dca1075d0683bf045c7dbd7a472e6a67dbddb1d
MisterX <weinboy11@gmail.com>
2021-06-03 22:58:11 +0200

    stuffs

--- src/main/java/kaptainwutax/seedcrackerX/mixin/GameRendererMixin.java
+++ src/main/java/kaptainwutax/seedcrackerX/mixin/GameRendererMixin.java
@@ -1,7 +1,6 @@
 package kaptainwutax.seedcrackerX.mixin;
 
 import kaptainwutax.seedcrackerX.finder.FinderQueue;
-import net.minecraft.client.render.BufferBuilderStorage;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
@@ -15,9 +14,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GameRenderer.class)
 public abstract class GameRendererMixin {
 
-    @Shadow
-    @Final
-    private BufferBuilderStorage buffers;
     @Shadow @Final private Camera camera;
 
     @Inject(method = "renderWorld", at = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/util/profiler/Profiler;swap(Ljava/lang/String;)V", args = {"ldc=hand"}))

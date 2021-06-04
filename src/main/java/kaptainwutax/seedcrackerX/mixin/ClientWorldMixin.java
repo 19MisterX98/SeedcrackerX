c84ff9252f2091dd05718448e17ae637a58d2d7b
MisterX <weinboy11@gmail.com>
2021-06-05 00:08:58 +0200

    maybe fix for cornos

--- src/main/java/kaptainwutax/seedcrackerX/mixin/ClientWorldMixin.java
+++ src/main/java/kaptainwutax/seedcrackerX/mixin/ClientWorldMixin.java
@@ -3,10 +3,14 @@ package kaptainwutax.seedcrackerX.mixin;
 import kaptainwutax.seedcrackerX.SeedCracker;
 import kaptainwutax.seedcrackerX.profile.config.ConfigScreen;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.registry.DynamicRegistryManager;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.biome.BuiltinBiomes;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,6 +18,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ClientWorld.class)
 public abstract class ClientWorldMixin {
+
+    @Shadow public abstract DynamicRegistryManager getRegistryManager();
 
     @Inject(method = "disconnect", at = @At("HEAD"))
     private void disconnect(CallbackInfo ci) {
@@ -23,7 +29,7 @@ public abstract class ClientWorldMixin {
 
     @Inject(method = "getGeneratorStoredBiome", at = @At("HEAD"), cancellable = true)
     private void getGeneratorStoredBiome(int x, int y, int z, CallbackInfoReturnable<Biome> ci) {
-        ci.setReturnValue(BuiltinBiomes.THE_VOID);
+        ci.setReturnValue(getRegistryManager().get(Registry.BIOME_KEY).getOrThrow(BiomeKeys.THE_VOID));
     }
 
 }

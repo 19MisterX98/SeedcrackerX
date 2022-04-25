abb7542b74c285bf50d16f391176a509fcaed7a1
CanadianBacon <beamconnor@gmail.com>
2022-04-24 21:06:07 -0700

    Port to Forge using Architectury
    Most changes described in README.md

--- src/main/java/kaptainwutax/seedcrackerX/SeedCracker.java
+++ src/main/java/kaptainwutax/seedcrackerX/SeedCracker.java
@@ -1,17 +1,13 @@
 package kaptainwutax.seedcrackerX;
 
-import kaptainwutax.seedcrackerX.api.SeedCrackerAPI;
 import kaptainwutax.seedcrackerX.config.Config;
 import kaptainwutax.seedcrackerX.cracker.storage.DataStorage;
 import kaptainwutax.seedcrackerX.finder.FinderQueue;
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
 
-import java.util.ArrayList;
-
-public class SeedCracker implements ModInitializer {
-
-    public static final ArrayList<SeedCrackerAPI> entrypoints = new ArrayList<>();
+@Mod("seedcrackerx")
+public class SeedCracker {
     private static SeedCracker INSTANCE;
     private final DataStorage dataStorage = new DataStorage();
 
@@ -19,13 +15,13 @@ public class SeedCracker implements ModInitializer {
         return INSTANCE;
     }
 
-    @Override
-    public void onInitialize() {
+    public SeedCracker()
+    {
         INSTANCE = this;
         Config.load();
         Features.init(Config.get().getVersion());
-        FabricLoader.getInstance().getEntrypointContainers("seedcrackerx", SeedCrackerAPI.class).forEach(entrypoint ->
-                entrypoints.add(entrypoint.getEntrypoint()));
+
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
     public DataStorage getDataStorage() {

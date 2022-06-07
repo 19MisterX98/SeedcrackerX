9d89cf3c85f6d176b62b9375b99e22e867af481b
misterx <weinboy11@gmail.com>
2022-06-07 23:30:23 +0200

    still need to fix gh actions

--- src/main/java/kaptainwutax/seedcrackerX/util/BiomeFixer.java
+++ src/main/java/kaptainwutax/seedcrackerX/util/BiomeFixer.java
@@ -2,92 +2,58 @@ package kaptainwutax.seedcrackerX.util;
 
 import com.seedfinding.mcbiome.biome.Biome;
 import com.seedfinding.mcbiome.biome.Biomes;
-import com.seedfinding.mccore.version.MCVersion;
-import kaptainwutax.seedcrackerX.config.Config;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BiomeFixer {
 
-    //now this is what I call a hack-fix
-    static final short[] idTransform = new short[61];
+    private static final Map<String, Biome> COMPATREGISTRY = new HashMap<>();
 
     static {
-        idTransform[0] = 127;
-        idTransform[1] = 1;
-        idTransform[2] = 129;
-        idTransform[3] = 12;
-        idTransform[4] = 140;
-        idTransform[5] = 2;
-        idTransform[6] = 6;
-        idTransform[7] = 4;
-        idTransform[8] = 132;
-        idTransform[9] = 27;
-        idTransform[10] = 29;
-        idTransform[11] = 155;
-        idTransform[12] = 32;
-        idTransform[13] = 160;
-        idTransform[14] = 5;
-        idTransform[15] = 30;
-        idTransform[16] = 35;
-        idTransform[17] = 36;
-        idTransform[18] = 3;
-        idTransform[19] = 131;
-        idTransform[20] = 34;
-        idTransform[21] = 163;
-        idTransform[22] = 21;
-        idTransform[23] = 23;
-        idTransform[24] = 168;
-        idTransform[25] = 37;
-        idTransform[26] = 165;
-        idTransform[27] = 38;
-        idTransform[28] = 1; //meadow replaced by plains
-        idTransform[29] = 19; //grove replaced by taiga
-        idTransform[30] = 140; //snowy slopes replaced by snowy tundra
-        idTransform[31] = 19; //frozen peaks replaced by taiga
-        idTransform[32] = 19; //jagged peaks replaced by taiga
-        idTransform[33] = 19; //jagged peaks replaced by taiga
-        idTransform[34] = 7;
-        idTransform[35] = 11;
-        idTransform[36] = 16;
-        idTransform[37] = 26;
-        idTransform[38] = 25;
-        idTransform[39] = 44;
-        idTransform[40] = 45;
-        idTransform[41] = 48;
-        idTransform[42] = 0;
-        idTransform[43] = 24;
-        idTransform[44] = 46;
-        idTransform[45] = 49;
-        idTransform[46] = 10;
-        idTransform[47] = 50;
-        idTransform[48] = 14;
-        idTransform[49] = 174;
-        idTransform[50] = 175;
-        idTransform[51] = 8;
-        idTransform[52] = 172;
-        idTransform[53] = 171;
-        idTransform[54] = 170;
-        idTransform[55] = 173;
-        idTransform[56] = 9;
-        idTransform[57] = 42;
-        idTransform[58] = 41;
-        idTransform[59] = 40;
-        idTransform[60] = 43;
+        for (Biome biome : Biomes.REGISTRY.values()) {
+            COMPATREGISTRY.put(biome.getName(), biome);
+        }
+        //renamed
+        COMPATREGISTRY.put("snowy_plains", Biomes.SNOWY_TUNDRA);
+        COMPATREGISTRY.put("old_growth_birch_forest", Biomes.TALL_BIRCH_FOREST);
+        COMPATREGISTRY.put("old_growth_pine_taiga", Biomes.GIANT_TREE_TAIGA);
+        COMPATREGISTRY.put("old_growth_spruce_taiga", Biomes.GIANT_TREE_TAIGA);
+        COMPATREGISTRY.put("windswept_hills", Biomes.EXTREME_HILLS);
+        COMPATREGISTRY.put("windswept_forest", Biomes.WOODED_MOUNTAINS);
+        COMPATREGISTRY.put("windswept_gravelly_hills", Biomes.GRAVELLY_MOUNTAINS);
+        COMPATREGISTRY.put("windswept_savanna", Biomes.SHATTERED_SAVANNA);
+        COMPATREGISTRY.put("sparse_jungle", Biomes.JUNGLE_EDGE);
+        COMPATREGISTRY.put("stony_shore", Biomes.STONE_SHORE);
+        //new
+        COMPATREGISTRY.put("meadow", Biomes.PLAINS);
+        COMPATREGISTRY.put("grove", Biomes.TAIGA);
+        COMPATREGISTRY.put("snowy_slopes", Biomes.SNOWY_TUNDRA);
+        COMPATREGISTRY.put("frozen_peaks", Biomes.TAIGA);
+        COMPATREGISTRY.put("jagged_peaks", Biomes.TAIGA);
+        COMPATREGISTRY.put("stony_peaks", Biomes.TAIGA);
+        COMPATREGISTRY.put("mangrove_swamp", Biomes.SWAMP);
+
+        //unsure what to do with those, they'll return THE_VOID for now
+        //dripstone_caves
+        //lush_caves
+        //deep_dark
     }
 
     public static Biome swap(net.minecraft.world.biome.Biome biome) {
-        if (MinecraftClient.getInstance().getNetworkHandler() == null) {
-            return Biomes.VOID;
-        }
-        int biomeID = MinecraftClient.getInstance().getNetworkHandler()
-                .getRegistryManager().get(Registry.BIOME_KEY).getRawId(biome);
+        ClientPlayNetworkHandler networkHandler = MinecraftClient.getInstance().getNetworkHandler();
+        if (networkHandler == null) return Biomes.VOID;
 
-        if (biomeID < 61 && Config.get().getVersion().isNewerOrEqualTo(MCVersion.v1_18)) {
-            biomeID = idTransform[biomeID];
-        }
-        return Biomes.REGISTRY.get(biomeID);
+        Identifier biomeID = networkHandler.getRegistryManager().get(Registry.BIOME_KEY).getId(biome);
+
+        if (biomeID == null) return Biomes.THE_VOID;
+
+        return COMPATREGISTRY.getOrDefault(biomeID.getPath(), Biomes.VOID);
     }
 
     public static net.minecraft.world.biome.Biome swap(Biome biome) {

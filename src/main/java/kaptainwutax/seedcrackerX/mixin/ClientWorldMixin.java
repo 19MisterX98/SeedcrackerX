a3d581a85ed4e8fed39e6dfb1545c00ea28b426f
misterx <weinboy11@gmail.com>
2024-12-07 20:37:32 +0100

    port to 1.21.3

--- src/main/java/kaptainwutax/seedcrackerX/mixin/ClientWorldMixin.java
+++ src/main/java/kaptainwutax/seedcrackerX/mixin/ClientWorldMixin.java
@@ -7,7 +7,6 @@ import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.util.profiler.Profiler;
 import net.minecraft.world.MutableWorldProperties;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
@@ -19,13 +18,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.function.Supplier;
-
 @Mixin(ClientWorld.class)
 public abstract class ClientWorldMixin extends World {
 
-    protected ClientWorldMixin(MutableWorldProperties properties, RegistryKey<World> registryRef, DynamicRegistryManager registryManager, RegistryEntry<DimensionType> dimensionEntry, Supplier<Profiler> profiler, boolean isClient, boolean debugWorld, long biomeAccess, int maxChainedNeighborUpdates) {
-        super(properties, registryRef, registryManager, dimensionEntry, profiler, isClient, debugWorld, biomeAccess, maxChainedNeighborUpdates);
+    protected ClientWorldMixin(MutableWorldProperties properties, RegistryKey<World> registryRef, DynamicRegistryManager registryManager, RegistryEntry<DimensionType> dimensionEntry, boolean isClient, boolean debugWorld, long biomeAccess, int maxChainedNeighborUpdates) {
+        super(properties, registryRef, registryManager, dimensionEntry, isClient, debugWorld, biomeAccess, maxChainedNeighborUpdates);
     }
 
     @Inject(method = "disconnect", at = @At("HEAD"))
@@ -36,8 +33,7 @@ public abstract class ClientWorldMixin extends World {
 
     @Inject(method = "getGeneratorStoredBiome", at = @At("HEAD"), cancellable = true)
     private void getGeneratorStoredBiome(int x, int y, int z, CallbackInfoReturnable<RegistryEntry<Biome>> ci) {
-        var biome = getRegistryManager().get(RegistryKeys.BIOME).getEntry(BiomeKeys.THE_VOID);
+        var biome = getRegistryManager().getOrThrow(RegistryKeys.BIOME).getOptional(BiomeKeys.THE_VOID);
         biome.ifPresent(ci::setReturnValue);
     }
-
 }
--- src/main/java/kaptainwutax/seedcrackerX/mixin/GameRendererMixin.java
+++ src/main/java/kaptainwutax/seedcrackerX/mixin/GameRendererMixin.java
@@ -1,31 +1,23 @@
 package kaptainwutax.seedcrackerX.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import kaptainwutax.seedcrackerX.finder.FinderQueue;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.RenderTickCounter;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.Entity;
 import org.joml.Matrix4f;
-import org.joml.Quaternionf;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(GameRenderer.class)
 public abstract class GameRendererMixin {
 
-    @Shadow
-    @Final
-    private Camera camera;
-
-    @Inject(method = "renderWorld", at = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/util/profiler/Profiler;swap(Ljava/lang/String;)V", args = {"ldc=hand"}), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void renderWorldHand(RenderTickCounter tickCounter, CallbackInfo ci, float f, boolean bl, Camera camera, Entity entity, float g, double d, Matrix4f matrix4f, MatrixStack matrixStack, float h, float i, Quaternionf quaternionf, Matrix4f matrix4f2) {
-        FinderQueue.get().renderFinders(matrix4f2, camera);
+    @Inject(method = "renderWorld", at = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/util/profiler/Profiler;swap(Ljava/lang/String;)V", args = {"ldc=hand"}))
+    private void renderWorldHand(RenderTickCounter renderTickCounter, CallbackInfo ci, @Local Camera camera, @Local(ordinal = 2) Matrix4f matrix4f3) {
+        FinderQueue.get().renderFinders(matrix4f3, camera);
     }
 
 }
